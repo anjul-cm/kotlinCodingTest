ccdaf3eb7ad7db1998f5de2a911b63ae51f10da0
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-24 18:12:35 +0545

    start required activity from StartingActivity.kt checking logged in user status

--- app/src/main/java/com/codingmountain/kotlinCodingTest/ui/start/StartingActivity.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/ui/start/StartingActivity.kt
@@ -1,10 +1,31 @@
 package com.codingmountain.kotlincodingtest.ui.start
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.codingmountain.kotlincodingtest.ui.auth.AuthActivity
+import com.codingmountain.kotlincodingtest.ui.main.DashBoardActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class StartingActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startRequiredActivity()
     }
+
+    private fun startRequiredActivity() {
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivityFinishingCurrentOne(AuthActivity::class.java)
+        } else {
+            startActivityFinishingCurrentOne(DashBoardActivity::class.java)
+        }
+    }
+
+    private fun <T> startActivityFinishingCurrentOne(activity: Class<T>) {
+        startActivity(Intent(this, activity))
+        finish()
+    }
+
+
 }
