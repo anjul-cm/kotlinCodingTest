c94d376d13e54288eb49e432b00c46d0bc3c7707
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-24 18:19:41 +0545

    add LoginFragment.kt and SignUpFragment.kt

---
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/ui/auth/login/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.codingmountain.kotlincodingtest.ui.auth.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codingmountain.kotlincodingtest.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
