ef048276c531dd77b3cf37468b508e89972d87f3
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-24 22:01:52 +0545

    make dashboard activity inherit from base activity

--- app/src/main/java/com/codingmountain/kotlinCodingTest/ui/main/DashBoardActivity.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/ui/main/DashBoardActivity.kt
@@ -3,8 +3,9 @@ package com.codingmountain.kotlincodingtest.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.codingmountain.kotlincodingtest.databinding.ActivityDashBoardBinding
+import com.codingmountain.kotlincodingtest.ui.base.BaseActivity
 
-class DashBoardActivity : AppCompatActivity() {
+class DashBoardActivity : BaseActivity() {
     private lateinit var binding: ActivityDashBoardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
