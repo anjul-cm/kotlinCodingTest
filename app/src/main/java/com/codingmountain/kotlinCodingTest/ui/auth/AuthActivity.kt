ac84b9a8d343614bd30168175d0c1bcf81412535
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-24 18:22:31 +0545

    transact LoginFragment on create of AuthActivity

--- app/src/main/java/com/codingmountain/kotlinCodingTest/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/ui/auth/AuthActivity.kt
@@ -3,12 +3,22 @@ package com.codingmountain.kotlincodingtest.ui.auth
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.codingmountain.kotlincodingtest.databinding.ActivityAuthBinding
+import com.codingmountain.kotlincodingtest.ui.auth.login.LoginFragment
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.authActivityContainer.id, LoginFragment())
+                .commit()
+        }
+
         setContentView(binding.root)
     }
+
 }
