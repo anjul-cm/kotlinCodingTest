60a594a2a1c0a6a9259ef4dccc6878258bde9426
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-25 22:36:59 +0545

    add basic crud functions to StationDao.kt

--- app/src/main/java/com/codingmountain/kotlinCodingTest/database/dao/StationDao.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/database/dao/StationDao.kt
@@ -1,13 +1,25 @@
 package com.codingmountain.kotlincodingtest.database.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.codingmountain.kotlincodingtest.network.responses.stationresponse.ChargingStation
 
 @Dao
 interface StationDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(station: ChargingStation)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStationList(stationList: List<ChargingStation>)
+
+    @Query("Select * from ChargingStation")
+    fun getAllStationPagingData(): PagingSource<Int, ChargingStation>
+
+    @Query("Delete from ChargingStation")
+    suspend fun deleteAll()
+
 
 }
