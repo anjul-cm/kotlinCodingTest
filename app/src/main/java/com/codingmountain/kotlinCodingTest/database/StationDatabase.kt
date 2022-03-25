c43499a3279b2059d9a21956a62be011a5aa5a2f
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-25 22:30:54 +0545

    fix using StationDatabase as entity

--- app/src/main/java/com/codingmountain/kotlinCodingTest/database/StationDatabase.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/database/StationDatabase.kt
@@ -3,12 +3,13 @@ package com.codingmountain.kotlincodingtest.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.codingmountain.kotlincodingtest.database.dao.StationDao
+import com.codingmountain.kotlincodingtest.network.responses.stationresponse.ChargingStation
 
-@Database(entities = [StationDatabase::class], version = 1)
+@Database(entities = [ChargingStation::class], version = 1, exportSchema = false)
 abstract class StationDatabase : RoomDatabase() {
     abstract fun getStationDao(): StationDao
 
-    companion object{
+    companion object {
         const val DATABASE_NAME = "StationDatabase"
     }
 }
