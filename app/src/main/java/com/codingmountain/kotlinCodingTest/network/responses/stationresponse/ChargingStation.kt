23c3714df049ecc7abca56444aa0509efcff4b51
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-25 22:16:31 +0545

    make telephone the primary key

--- app/src/main/java/com/codingmountain/kotlinCodingTest/network/responses/stationresponse/ChargingStation.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/network/responses/stationresponse/ChargingStation.kt
@@ -5,14 +5,14 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class ChargingStation(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
     val address: String,
     val city: String,
     val latitude: String,
     val longitude: String,
     val name: String,
     val province: String,
+
+    @PrimaryKey(autoGenerate = false)
     val telephone: String,
     val type: List<String>
 )
