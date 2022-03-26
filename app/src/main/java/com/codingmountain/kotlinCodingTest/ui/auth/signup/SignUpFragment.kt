21448e24859a7161e19b4560614bc7b9d5514d63
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-26 12:28:49 +0545

    check whether the passwords match while signing up

--- app/src/main/java/com/codingmountain/kotlinCodingTest/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/ui/auth/signup/SignUpFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.codingmountain.kotlincodingtest.databinding.FragmentSignUpBinding
@@ -29,11 +30,20 @@ class SignUpFragment : Fragment() {
 
     private fun setClickListenerToSignUpBtn() {
         binding.signUpFrgSignUpBtn.setOnClickListener {
-            activityViewModel.signUp(
-                binding.signupFrgEmailET.text.toString(),
-                binding.signUpFrgPasswordTV.text.toString()
-            )
+            if (passwordMatches()) {
+                activityViewModel.signUp(
+                    binding.signUpFrgEmailET.text.toString(),
+                    binding.signUpFrgPasswordET.text.toString()
+                )
+            } else {
+                Toast.makeText(requireContext(), "Passwords do not match.", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
+    }
+
+    private fun passwordMatches(): Boolean {
+        return binding.signUpFrgPasswordET.text.toString() == binding.signUpFrgReEnterPasswordET.text.toString()
     }
 
     private fun setClickListenerToSignInTextView() {
