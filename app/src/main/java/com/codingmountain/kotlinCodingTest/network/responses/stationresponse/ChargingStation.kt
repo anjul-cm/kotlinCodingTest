af49ab023b3917323085df610fa7e4b41ee28fda
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-03-26 13:36:20 +0545

    made longitude and latitude primary key

--- app/src/main/java/com/codingmountain/kotlinCodingTest/network/responses/stationresponse/ChargingStation.kt
+++ app/src/main/java/com/codingmountain/kotlinCodingTest/network/responses/stationresponse/ChargingStation.kt
@@ -1,21 +1,23 @@
 package com.codingmountain.kotlincodingtest.network.responses.stationresponse
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.codingmountain.kotlincodingtest.database.typeconverter.StringListToStringConverter
 
-@Entity
+@Entity(primaryKeys = ["latitude", "longitude"])
 @TypeConverters(StringListToStringConverter::class)
 data class ChargingStation(
     val address: String,
     val city: String,
+    @ColumnInfo(name = "latitude")
     val latitude: String,
+
+    @ColumnInfo(name = "longitude")
     val longitude: String,
     val name: String,
     val province: String,
 
-    @PrimaryKey(autoGenerate = false)
     val telephone: String,
     val type: List<String>
 )
